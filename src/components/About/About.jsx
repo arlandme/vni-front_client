115d6f139b59006bdc03c87f86027d0fea9c3015
arlandme <arland.me@outlook.com>
2022-12-23 22:35:55 +0700

    update 23/12

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -32,6 +32,7 @@ export default function About() {
             Initial production check (IPC), During production check (DUPRO), Pre-shipment inspection (PSI), Container loading supervision (LS), 
             Sample drawing (SD) and Full Inspection (FI).
           </p>
+          <br>
           <p className='pb-4'>
             Vietnam Inspection Team is a perfect choice for quality control and the best partner of inspection service companies.
           </p>
