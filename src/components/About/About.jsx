7998f17663bf7df171e7516d558488141029e884
arlandme <arland.me@outlook.com>
2022-12-23 22:16:39 +0700

    update 23/12

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -14,11 +14,22 @@ export default function About() {
             as strictly and systematically control product quality based on objectivity, transparency and accuracy.
           </p>
           <p className='pb-4'>
-            With a team of professional, qualified and experienced inspectors, we
-            provide our customers with the best quality control solution in
-            every service and can do the full inspection services: Initial
-            production check (IPC), During production check (DUPRO),
-            Pre-shipment inspection (PSI), Container loading supervision (LS),
+            Vietnam Inspection Team has two offices which perfectly support the inspection services
+            and fully cover the northern areas. Strong industrialization will spread in Northern Vietnam in the future,
+            therefore we set up an office in Hung Yen to easily cover the inspections in northern provinces with large industrial zones.  
+            Vietnam is oriented to focus on pushing industrialization development with available advantageous conditions, 
+            strong industrial development will lead to high requirements of quality and create more service segments in the inspection 
+            field due to increasing customers’ needs and various product categories. When industry in Northern Vietnam thrives, 
+            customers will use professional inspection services that are suitable for their costs and products.  Seeing this in the future, 
+            Vietnam Inspection Team was established to catch this trend to help customers save cost as well as time with professionalism 
+            in each service.
+          </p>
+          <p className=''>
+            Our teammates who come from huge famous inspection companies have various experience and skills. 
+            Working with us, customers totally experience good services which are created by combination of advantages 
+            from top inspection service companies. With a team of professional, qualified, experienced and local inspectors, 
+            we provide our customers with the best quality control solution in every service and can do the full inspection services: 
+            Initial production check (IPC), During production check (DUPRO), Pre-shipment inspection (PSI), Container loading supervision (LS), 
             Sample drawing (SD) and Full Inspection (FI).
           </p>
           <p className=''>
