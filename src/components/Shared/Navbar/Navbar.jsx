2027f7a4a13ef1d5143d821e0e78c99910e60390
arlandme <arland.me@outlook.com>
2023-01-10 14:18:00 +0700

    updated 10/01

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -64,8 +64,7 @@ export default function Navbar({ services, products }) {
                 src={require('../../../assets/img/logos/logo.png')}
                 alt='logo vninspection'
               />
-              <div className='slogan text-sm text-left text-slate-50 border-t mt-1 pt-1'>
-                
+
               </div>
             </Link>
             <button
