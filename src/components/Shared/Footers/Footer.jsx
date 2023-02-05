0fc185aa8eb0373990387ea02a601033732bf539
arlandme <arland.me@outlook.com>
2023-02-05 16:42:41 +0700

    up 5/2

--- src/components/Shared/Footers/Footer.jsx
+++ src/components/Shared/Footers/Footer.jsx
@@ -193,7 +193,7 @@ export default function Footer() {
                   rel='noopener noreferrer'
                   className='text-slate-100 hover:text-slate-200'
                 >
-                  HandiPassion JSC
+                  HandiPassion Joint Stock Company
                 </a>
               </div>
             </div>
