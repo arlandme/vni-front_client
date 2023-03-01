f9529254a065a7b0bb4651fb60ac4181a56aa47b
arlandme <arland.me@outlook.com>
2023-03-01 22:50:53 +0700

    up 1/3

--- src/assets/data/about.js
+++ src/assets/data/about.js
@@ -50,7 +50,7 @@ export const why = {
         'To be qualified with ISO 17020:2012 & ISO 9001:2015 and specialize in Hardline, Softline and Electrical & Electronics.',
     },
     {
-      title: 'Good Price & Good Services',
+      title: 'Good & Good',
       content:
         'Working professionally, precisely and swiftly is a mandatory element for sustainable development. To win customers’ satisfaction, we have not only the best services but also the best price.',
     },
@@ -69,7 +69,7 @@ export const about = {
   // Thêm đoạn văn ở đây
   contents: [
     'Established in the context of the global economic crisis, InsQuality understands the difficulties in sales of customers and the delay in mass production of factories. We were born with the mission to help customers’ sales go smoothly as well as strictly and systematically control product quality based on objectivity, transparency and accuracy.',
-    'InsQuality which was created as a brand of HandiPassion JSC in 2023 functions as a quality management team for handicraft products manufactured by HandiPassion with the process of strict and flexible control to ensure fewest defects can exist. We have two offices which perfectly support the inspection services and fully cover the northern areas. Strong industrialization will spread in Northern Vietnam in the future, therefore we set up an office in Hung Yen to easily cover the inspections in northern provinces with large industrial zones. Vietnam is oriented to focus on pushing industrialization development with available advantageous conditions, strong industrial development will lead to high requirements of quality and create more service segments in the inspection field due to increasing customers’ needs and various product categories. When industry in Northern Vietnam thrives, customers will use professional inspection services that are suitable for their costs and products. Seeing this in the future, InsQuality was established to catch this trend to help customers save cost as well as time with professionalism in each service.',
+    'InsQuality which was created as a brand of HandiPassion JSC in 2023 functions as a quality management team for handicraft products manufactured by HandiPassion with the process of strict and flexible control to ensure fewest defects can exist. We have two offices which perfectly support the inspection services and fully cover the northern areas. InsQuality was established to help customers save cost as well as time with professionalism in each service.',
     'Being qualified with ISO 17020:2012 and ISO 9001:2015, 100% our QC inspectors who graduated from technical universities with Bachelor of Engineering and good English skills. Their excellences at every industry are dedicated to bring the best inspection services for customers. With a team of professional, qualified, experienced and local inspectors, we provide our customers with the best quality control solution in every service and can do the full inspection services: Initial production check (IPC), During production check (DUPRO), Pre-shipment inspection (PSI), Container loading supervision (LS), Sample drawing (SD), Full Inspection (FI) and Factory audit (FA).',
     'We are a perfect choice for customers who look for a trustworthy inspection company to protect their brands.',
   ],
